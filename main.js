aca117e35775866ca206db5a62757b0f18848f37
Oualid <atiyawalid1@gmail.com>
2024-12-13 16:42:05 +0100

    Add About Page And modefied Some javascript

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
+const mainCtn = document.querySelector('.main-container');
 const xBtn = document.getElementById('x-btn');
 const menuBtn = document.getElementById('menu-btn');
 const nav = document.querySelector('nav');
 menuBtn.addEventListener('click' , () => {
+    mainCtn.style = 'height:100vh;overflow-y:hidden';
     nav.style = 'margin-right:initial';
 })
 xBtn.addEventListener('click' , () => {
     nav.style = 'margin-right:-500px';
+    mainCtn.style = 'height:initial;overflow-y:initial';
+
 })
 
 var typed = new Typed('.input',{
-    strings :['Front End Developer','Back End Developer'] ,
-    typeSpeed : 70,
+    strings :['Oualid Atiya','A Web Developer'] ,
+    loop:true,
+    typeSpeed : 55,
     backSpeed  : 55,
-    loop : true
+    startDelay: 500,
+    backDelay: 2000,
+})
+
+const navlinks = document.querySelectorAll('.ctn-links a');
+navlinks.forEach(link => {
+    link.addEventListener('click', () => {
+        nav.style = 'margin-right:-500px';
+        mainCtn.style = 'height:initial;overflow-y:initial';
+    })
 })
